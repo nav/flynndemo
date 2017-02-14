294f642e9d44c15458e2ac3b21d2c442b0cffb1c
Nav Aulakh <nav@navaulakh.com>
2017-02-13 22:48:45 -0800

    Working with staticfiles

--- inventory/settings.py
+++ inventory/settings.py
@@ -129,10 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/media/inventory'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+STATIC_ROOT = '/tmp'
 
 
 SITE_ID = 1
